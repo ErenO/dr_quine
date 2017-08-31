94bde7e3d896ed954126b7b490fe69669620276f
Eren OZDEK <eozdek@e3r3p20.42.fr>
2017-08-31 15:58:22 +0200

    projet au propre

--- C/Grace.c
+++ C/Grace.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
 #define FIRST
 #define SECOND
-#define THIRD(x) int main(){FILE*file = fopen("Grace_kid.c", "w");char*a="#include<stdio.h>%1$c#define FIRST%1$c#define SECOND%1$c#define THIRD(x) int main(){FILE*file = fopen(%3$cGrace_kid.c%3$c, %3$cw%3$c);char*a=%3$c%2$s%3$c;fprintf(file,a,10,a,34);}%1$cTHIRD()%1$c";fprintf(file,a,10,a,34);}
+#define THIRD(x) int main(){FILE*file = fopen("Grace_kid.c", "w");char*s="#include<stdio.h>%1$c#define FIRST%1$c#define SECOND%1$c#define THIRD(x) int main(){FILE*file = fopen(%3$cGrace_kid.c%3$c, %3$cw%3$c);char*s=%3$c%2$s%3$c;fprintf(file,s,10,s,34);}%1$cTHIRD()%1$c";fprintf(file,s,10,s,34);}
 THIRD()
