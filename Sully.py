b77c86bfda825dbf688127485b56d903382298ac
Eren OZDEK <eozdek@e2r1p1.42.fr>
2018-03-02 22:15:39 +0100

    sully presque finit

--- Sully.py
+++ Sully.py
@@ -1,5 +1,4 @@
 import os
-import sys
 
 def main():
     i = 12
@@ -8,9 +7,8 @@ def main():
     str = "Sully_%d.py" % i
     f1 = open(str, "w")
     i -= 1
-    stri = str(i)
-    s = r"print>>f1, 'import os\n\ndef main():\n\ti = ' + stri + '\n\tif (i <= 0):\n\t\treturn (0)\n\ts = r\"' + s + '\"'+'\n\texec(s)\nmain()'"
+    s = r"print>>f1, 'import os\n\ndef main():\n\ti = 11\n\tif (i <= 0):\n\t\treturn (0)\n\tstr = \"Sully_%d.py\" % i\n\tf1 = open(str, \"w\")\n\ti -= 1\n\ts = r\"' + s + '\"\n\tf1.close()\n\tcommand = \"python %s\" % (str)\n\tos.system(command)\n\texec(s)\nmain()'"
     command = "python %s" % (str)
     os.system(command)
-    exec(s)
+    exec(third)
 main()
